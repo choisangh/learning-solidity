e0fb03dc4272865a7e5701689c4c21647c739bb9
choisangh <fhqhxm114@gmail.com>
2022-11-26 09:28:28 +0900

    whitelist

---
+++ BNB_Chain_chellange/whitelist.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MintNFT is ERC721Enumerable, Ownable {
+    uint constant public TOTAL_NFT = 100;
+    string public metadataURI;
+    string public notRevealedURI;
+    bool public isRevealed;
+    mapping(address => uint) whitelist;
+
+    constructor(string memory _metadataURI, string memory _notRevealedURI) ERC721("BNB,Challenger", "BC") {
+        metadataURI = _metadataURI;
+        notRevealedURI = _notRevealedURI;
+    }
+
+    function mintNFT() public payable { //payable
+        require(TOTAL_NFT > totalSupply(), "No more mint.");
+        require(whitelist[msg.sender]>0,"Not whitelist.");
+        require(msg.value >= 0.1 ether, "Not Enough BNB.");
+
+        uint tokenId = totalSupply() + 1;
+
+        _mint(msg.sender, tokenId);
+
+        whitelist[msg.sender]--; // whitelist[msg.sender] = whitelist[msg.sennder] -1;
+
+        payable(owner()).transfer(msg.value); //owner -> smart contract 배포자
+
+        //payable(owner()).transfer(수수료);
+        //payable(판매자).transfer(msg.value-수수료);
+
+        
+    }
+
+    function batchMintNFT(uint _amount) public {
+        for(uint i = 0; i < _amount; i++) {
+            mintNFT();
+        }
+    }
+
+    function tokenURI(uint _tokenId) public override view returns (string memory) {
+        if(isRevealed == false) return notRevealedURI;
+
+        return string(abi.encodePacked(metadataURI, '/', Strings.toString(_tokenId), '.json'));
+    }
+
+    function reveal() public onlyOwner {
+        isRevealed = true;
+    }
+
+    function setWhitelist(address _whitelist, uint _amount) public onlyOwner{ //onlyOwner -> 배포자만 실행가능
+        whitelist[_whitelist] = _amount;
+    }
+}
